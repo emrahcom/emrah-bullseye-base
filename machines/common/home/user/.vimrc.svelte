4114c72cf1e49a3ffeb04e1fa318eb53ddb3b5c7
emrah <emrah.com@gmail.com>
2022-02-03 20:19:31 +0300

    feat: add vim config templates

---
+++ machines/common/home/user/.vimrc.svelte
@@ -0,0 +1,17 @@
+
+autocmd BufNewFile,BufRead *.svelte call Svelte()
+function Svelte()
+  setlocal
+  \ textwidth=80
+  \ tabstop=2
+  \ shiftwidth=2
+  \ softtabstop=2
+  \ expandtab
+  \ autoindent
+  \ nosmartindent
+  \ nocindent
+  \ list
+  \ listchars=tab:»·,trail:·
+  \ filetype=html
+  \ syntax=html
+endfunction
