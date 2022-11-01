9ece16bea1a64ce1c1b9fc922958241e6301567f
emrah <emrah.eryilmaz@nordeck.net>
2022-11-01 17:29:56 +0300

    refactor: renamed APT_PROXY in host custom

--- installer-sub-scripts/common/100-host-custom.sh
+++ installer-sub-scripts/common/100-host-custom.sh
@@ -26,17 +26,17 @@ echo "---------------------- HOST CUSTOM ------------------------"
 export DEBIAN_FRONTEND=noninteractive
 
 # upgrade
-apt-get $APT_PROXY_OPTION -yd dist-upgrade
-apt-get $APT_PROXY_OPTION -y upgrade
+apt-get $APT_PROXY -yd dist-upgrade
+apt-get $APT_PROXY -y upgrade
 
 # added packages
-apt-get $APT_PROXY_OPTION -y install zsh tmux vim autojump
-apt-get $APT_PROXY_OPTION -y install htop iotop bmon bwm-ng
-apt-get $APT_PROXY_OPTION -y install fping whois
-apt-get $APT_PROXY_OPTION -y install net-tools ngrep ncat
-apt-get $APT_PROXY_OPTION -y install wget curl rsync
-apt-get $APT_PROXY_OPTION -y install bzip2 ack jq
-apt-get $APT_PROXY_OPTION -y install rsyslog
+apt-get $APT_PROXY -y install zsh tmux vim autojump
+apt-get $APT_PROXY -y install htop iotop bmon bwm-ng
+apt-get $APT_PROXY -y install fping whois
+apt-get $APT_PROXY -y install net-tools ngrep ncat
+apt-get $APT_PROXY -y install wget curl rsync
+apt-get $APT_PROXY -y install bzip2 ack jq
+apt-get $APT_PROXY -y install rsyslog
 
 # ------------------------------------------------------------------------------
 # ROOT USER
