0ec0b41c03b42d36acfc6302daf799bb4cab6bb3
emrah <emrah.eryilmaz@nordeck.net>
2022-12-08 12:03:27 +0300

    refactor: rename as dummy0

--- installer-sub-scripts/common/100-network.sh
+++ installer-sub-scripts/common/100-network.sh
@@ -131,7 +131,7 @@ sed -i "s/___BRIDGE___/${BRIDGE}/g" /etc/network/interfaces.d/eb-bridge.cfg
 cp etc/dnsmasq.d/eb-interface /etc/dnsmasq.d/
 sed -i "s/___BRIDGE___/${BRIDGE}/g" /etc/dnsmasq.d/eb-interface
 
-ifup -i /etc/network/interfaces.d/eb-bridge.cfg edummy0
+ifup -i /etc/network/interfaces.d/eb-bridge.cfg dummy0
 ifup -i /etc/network/interfaces.d/eb-bridge.cfg $BRIDGE
 
 # ------------------------------------------------------------------------------
