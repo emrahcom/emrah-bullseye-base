47f452b2a22b1080a640956b307d66769698c98d
emrah <emrah.eryilmaz@nordeck.net>
2022-12-08 14:12:40 +0300

    refactor: TAG in network config

--- installer-sub-scripts/eb-base/130-network.sh
+++ installer-sub-scripts/eb-base/130-network.sh
@@ -9,7 +9,7 @@ source $INSTALLER/000-source
 # ------------------------------------------------------------------------------
 # ENVIRONMENT
 # ------------------------------------------------------------------------------
-MACH="eb-host"
+MACH="$TAG-host"
 cd $MACHINES/$MACH
 
 # public interface
@@ -19,7 +19,7 @@ PUBLIC_INTERFACE=${PUBLIC_INTERFACE/% */}
 echo PUBLIC_INTERFACE="$PUBLIC_INTERFACE" >> $INSTALLER/000-source
 
 # IP address (IP used in the private bridge network)
-DNS_RECORD=$(grep 'address=/host/' etc/dnsmasq.d/eb-hosts | head -n1)
+DNS_RECORD=$(grep 'address=/host/' etc/dnsmasq.d/$TAG-hosts | head -n1)
 IP=${DNS_RECORD##*/}
 echo HOST="$IP" >> $INSTALLER/000-source
 
@@ -43,13 +43,13 @@ echo "------------------------ NETWORK --------------------------"
 # ------------------------------------------------------------------------------
 # BACKUP & STATUS
 # ------------------------------------------------------------------------------
-OLD_FILES="/root/eb-old-files/$DATE"
+OLD_FILES="/root/$TAG-old-files/$DATE"
 mkdir -p $OLD_FILES
 
 # backup the files which will be changed
 [[ -f /etc/nftables.conf ]] && cp /etc/nftables.conf $OLD_FILES/
 [[ -f /etc/network/interfaces ]] && cp /etc/network/interfaces $OLD_FILES/
-[[ -f /etc/dnsmasq.d/eb-hosts ]] && cp /etc/dnsmasq.d/eb-hosts $OLD_FILES/
+[[ -f /etc/dnsmasq.d/$TAG-hosts ]] && cp /etc/dnsmasq.d/$TAG-hosts $OLD_FILES/
 [[ -f /etc/default/dnsmasq ]] && cp /etc/default/dnsmasq $OLD_FILES/
 [[ -f /etc/default/lxc-net ]] && cp /etc/default/lxc-net $OLD_FILES/
 
@@ -78,38 +78,38 @@ apt-get $APT_PROXY -y install nftables
 # NETWORK CONFIG
 # ------------------------------------------------------------------------------
 # changed/added system files
-cp etc/dnsmasq.d/eb-hosts /etc/dnsmasq.d/
-cp etc/dnsmasq.d/eb-resolv /etc/dnsmasq.d/
+cp etc/dnsmasq.d/$TAG-hosts /etc/dnsmasq.d/
+cp etc/dnsmasq.d/$TAG-resolv /etc/dnsmasq.d/
 [[ -z "$(egrep '^DNSMASQ_EXCEPT' /etc/default/dnsmasq)" ]] && \
     sed -i "s/^#DNSMASQ_EXCEPT/DNSMASQ_EXCEPT/" /etc/default/dnsmasq
 
 # /etc/network/interfaces
-[[ -z "$(egrep '^source-directory\s*interfaces.d' /etc/network/interfaces || true)" ]] && \
-[[ -z "$(egrep '^source-directory\s*/etc/network/interfaces.d' /etc/network/interfaces || true)" ]] && \
-[[ -z "$(egrep '^source\s*interfaces.d/\*$' /etc/network/interfaces || true)" ]] && \
-[[ -z "$(egrep '^source\s*/etc/network/interfaces.d/\*$' /etc/network/interfaces || true)" ]] && \
-[[ -z "$(egrep '^source\s*interfaces.d/\*\.cfg' /etc/network/interfaces || true)" ]] && \
-[[ -z "$(egrep '^source\s*/etc/network/interfaces.d/\*\.cfg' /etc/network/interfaces || true)" ]] && \
-[[ -z "$(egrep '^source\s*interfaces.d/eb-bridge.cfg' /etc/network/interfaces || true)" ]] && \
-[[ -z "$(egrep '^source\s*/etc/network/interfaces.d/eb-bridge.cfg' /etc/network/interfaces || true)" ]] && \
-echo -e "\nsource /etc/network/interfaces.d/eb-bridge.cfg" >> /etc/network/interfaces
+[[ -z "$(egrep "^source-directory\s*interfaces.d" /etc/network/interfaces || true)" ]] && \
+[[ -z "$(egrep "^source-directory\s*/etc/network/interfaces.d" /etc/network/interfaces || true)" ]] && \
+[[ -z "$(egrep "^source\s*interfaces.d/\*$" /etc/network/interfaces || true)" ]] && \
+[[ -z "$(egrep "^source\s*/etc/network/interfaces.d/\*$" /etc/network/interfaces || true)" ]] && \
+[[ -z "$(egrep "^source\s*interfaces.d/\*\.cfg" /etc/network/interfaces || true)" ]] && \
+[[ -z "$(egrep "^source\s*/etc/network/interfaces.d/\*\.cfg" /etc/network/interfaces || true)" ]] && \
+[[ -z "$(egrep "^source\s*interfaces.d/$TAG-bridge.cfg" /etc/network/interfaces || true)" ]] && \
+[[ -z "$(egrep "^source\s*/etc/network/interfaces.d/$TAG-bridge.cfg" /etc/network/interfaces || true)" ]] && \
+echo -e "\nsource /etc/network/interfaces.d/$TAG-bridge.cfg" >> /etc/network/interfaces
 
 # /etc/network/cloud-interfaces-template
 if [[ -f "/etc/network/cloud-interfaces-template" ]]; then
-    [[ -z "$(egrep '^source-directory\s*interfaces.d' /etc/network/cloud-interfaces-template || true)" ]] && \
-    [[ -z "$(egrep '^source-directory\s*/etc/network/interfaces.d' /etc/network/cloud-interfaces-template || true)" ]] && \
-    [[ -z "$(egrep '^source\s*interfaces.d/\*$' /etc/network/cloud-interfaces-template || true)" ]] && \
-    [[ -z "$(egrep '^source\s*/etc/network/interfaces.d/\*$' /etc/network/cloud-interfaces-template || true)" ]] && \
-    [[ -z "$(egrep '^source\s*interfaces.d/\*\.cfg' /etc/network/cloud-interfaces-template || true)" ]] && \
-    [[ -z "$(egrep '^source\s*/etc/network/interfaces.d/\*\.cfg' /etc/network/cloud-interfaces-template || true)" ]] && \
-    [[ -z "$(egrep '^source\s*interfaces.d/eb-bridge.cfg' /etc/network/cloud-interfaces-template || true)" ]] && \
-    [[ -z "$(egrep '^source\s*/etc/network/interfaces.d/eb-bridge.cfg' /etc/network/cloud-interfaces-template || true)" ]] && \
-    echo -e "\nsource /etc/network/interfaces.d/eb-bridge.cfg" >> /etc/network/cloud-interfaces-template
+    [[ -z "$(egrep "^source-directory\s*interfaces.d" /etc/network/cloud-interfaces-template || true)" ]] && \
+    [[ -z "$(egrep "^source-directory\s*/etc/network/interfaces.d" /etc/network/cloud-interfaces-template || true)" ]] && \
+    [[ -z "$(egrep "^source\s*interfaces.d/\*$" /etc/network/cloud-interfaces-template || true)" ]] && \
+    [[ -z "$(egrep "^source\s*/etc/network/interfaces.d/\*$" /etc/network/cloud-interfaces-template || true)" ]] && \
+    [[ -z "$(egrep "^source\s*interfaces.d/\*\.cfg" /etc/network/cloud-interfaces-template || true)" ]] && \
+    [[ -z "$(egrep "^source\s*/etc/network/interfaces.d/\*\.cfg" /etc/network/cloud-interfaces-template || true)" ]] && \
+    [[ -z "$(egrep "^source\s*interfaces.d/$TAG-bridge.cfg" /etc/network/cloud-interfaces-template || true)" ]] && \
+    [[ -z "$(egrep "^source\s*/etc/network/interfaces.d/$TAG-bridge.cfg" /etc/network/cloud-interfaces-template || true)" ]] && \
+    echo -e "\nsource /etc/network/interfaces.d/$TAG-bridge.cfg" >> /etc/network/cloud-interfaces-template
 fi
 
 # IP forwarding
-cp etc/sysctl.d/eb-ip-forward.conf /etc/sysctl.d/
-sysctl -p /etc/sysctl.d/eb-ip-forward.conf || true
+cp etc/sysctl.d/$TAG-ip-forward.conf /etc/sysctl.d/
+sysctl -p /etc/sysctl.d/$TAG-ip-forward.conf || true
 [[ "$(cat /proc/sys/net/ipv4/ip_forward)" != 1 ]] && false
 
 # ------------------------------------------------------------------------------
@@ -124,82 +124,82 @@ systemctl restart lxc-net.service
 # the random MAC address for the dummy interface
 MAC_ADDRESS=$(date +'52:54:%d:%H:%M:%S')
 
-cp etc/network/interfaces.d/eb-bridge.cfg /etc/network/interfaces.d/
+cp etc/network/interfaces.d/$TAG-bridge.cfg /etc/network/interfaces.d/
 sed -i "s/___MAC_ADDRESS___/${MAC_ADDRESS}/g" \
-    /etc/network/interfaces.d/eb-bridge.cfg
-sed -i "s/___BRIDGE___/${BRIDGE}/g" /etc/network/interfaces.d/eb-bridge.cfg
-cp etc/dnsmasq.d/eb-interface /etc/dnsmasq.d/
-sed -i "s/___BRIDGE___/${BRIDGE}/g" /etc/dnsmasq.d/eb-interface
+    /etc/network/interfaces.d/$TAG-bridge.cfg
+sed -i "s/___BRIDGE___/${BRIDGE}/g" /etc/network/interfaces.d/$TAG-bridge.cfg
+cp etc/dnsmasq.d/$TAG-interface /etc/dnsmasq.d/
+sed -i "s/___BRIDGE___/${BRIDGE}/g" /etc/dnsmasq.d/$TAG-interface
 
-ifup -i /etc/network/interfaces.d/eb-bridge.cfg dummy0
-ifup -i /etc/network/interfaces.d/eb-bridge.cfg $BRIDGE
+ifup -i /etc/network/interfaces.d/$TAG-bridge.cfg dummy0
+ifup -i /etc/network/interfaces.d/$TAG-bridge.cfg $BRIDGE
 
 # ------------------------------------------------------------------------------
 # NFTABLES
 # ------------------------------------------------------------------------------
 # recreate the custom tables
 if [[ "$RECREATE_CUSTOM_NFTABLES" = true ]]; then
-    nft delete table inet eb-filter 2>/dev/null || true
-    nft delete table ip eb-nat 2>/dev/null || true
+    nft delete table inet $TAG-filter 2>/dev/null || true
+    nft delete table ip $TAG-nat 2>/dev/null || true
 fi
 
-# table: eb-filter
+# table: $TAG-filter
 # chains: input, forward, output
 # rules: drop from the public interface to the private internal network
-nft add table inet eb-filter
-nft add chain inet eb-filter \
+nft add table inet $TAG-filter
+nft add chain inet $TAG-filter \
     input { type filter hook input priority 0 \; }
-nft add chain inet eb-filter \
+nft add chain inet $TAG-filter \
     forward { type filter hook forward priority 0 \; }
-nft add chain inet eb-filter \
+nft add chain inet $TAG-filter \
     output { type filter hook output priority 0 \; }
-[[ -z "$(nft list chain inet eb-filter output | \
+[[ -z "$(nft list chain inet $TAG-filter output | \
 ack 'ip daddr 172.22.22.0/24 drop')" ]] && \
-    nft add rule inet eb-filter output \
+    nft add rule inet $TAG-filter output \
     iif $PUBLIC_INTERFACE ip daddr 172.22.22.0/24 drop
 
-# table: eb-nat
+# table: $TAG-nat
 # chains: prerouting, postrouting, output, input
 # rules: masquerade
-nft add table ip eb-nat
-nft add chain ip eb-nat prerouting \
+nft add table ip $TAG-nat
+nft add chain ip $TAG-nat prerouting \
     { type nat hook prerouting priority 0 \; }
-nft add chain ip eb-nat postrouting \
+nft add chain ip $TAG-nat postrouting \
     { type nat hook postrouting priority 100 \; }
-nft add chain ip eb-nat output \
+nft add chain ip $TAG-nat output \
     { type nat hook output priority 0 \; }
-nft add chain ip eb-nat input \
+nft add chain ip $TAG-nat input \
     { type nat hook input priority 0 \; }
-[[ -z "$(nft list chain ip eb-nat postrouting | \
+[[ -z "$(nft list chain ip $TAG-nat postrouting | \
 ack 'ip saddr 172.22.22.0/24 masquerade')" ]] && \
-    nft add rule ip eb-nat postrouting \
+    nft add rule ip $TAG-nat postrouting \
     ip saddr 172.22.22.0/24 masquerade
 
-# table: eb-nat
+# table: $TAG-nat
 # chains: prerouting
 # maps: tcp2ip, tcp2port
 # rules: tcp dnat
-nft add map ip eb-nat tcp2ip \
+nft add map ip $TAG-nat tcp2ip \
     { type inet_service : ipv4_addr \; }
-nft add map ip eb-nat tcp2port \
+nft add map ip $TAG-nat tcp2port \
     { type inet_service : inet_service \; }
-[[ -z "$(nft list chain ip eb-nat prerouting | \
+[[ -z "$(nft list chain ip $TAG-nat prerouting | \
 ack 'tcp dport map @tcp2ip:tcp dport map @tcp2port')" ]] && \
-    nft add rule ip eb-nat prerouting \
+    nft add rule ip $TAG-nat prerouting \
     iif $PUBLIC_INTERFACE dnat \
     tcp dport map @tcp2ip:tcp dport map @tcp2port
 
-# table: eb-nat
+# table: $TAG-nat
 # chains: prerouting
 # maps: udp2ip, udp2port
 # rules: udp dnat
-nft add map ip eb-nat udp2ip \
+nft add map ip $TAG-nat udp2ip \
     { type inet_service : ipv4_addr \; }
-nft add map ip eb-nat udp2port \
+nft add map ip $TAG-nat udp2port \
     { type inet_service : inet_service \; }
-[[ -z "$(nft list chain ip eb-nat prerouting | \
+[[ -z "$(nft list chain ip $TAG-nat prerouting | \
 ack 'udp dport map @udp2ip:udp dport map @udp2port')" ]] && \
-    nft add rule ip eb-nat prerouting \
+    nft add rule ip $TAG-nat prerouting \
     iif $PUBLIC_INTERFACE dnat \
     udp dport map @udp2ip:udp dport map @udp2port
 
